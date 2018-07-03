b0c47610af0fe181b25110692b13681fbb5064ac
Polly <krvtzz@gmail.com>
2018-07-04 00:55:18 +0300

    init item

---
+++ components/Item.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='item'>
+      <nuxt-link :to="'path/'+item.id">
+        <h1>{{item.id}}</h1>
+
+      </nuxt-link>
+      <div>
+          {{item.gender}}
+      </div>
+      <p>{{item.email}}</p>
+    </div>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      itemData:{
+        type: Object
+      },
+    },
+    data(){
+      return{
+        item: this.itemData
+      }
+    }
+  }
+</script>
+
+<style>
+  .item {
+    padding: 20px;
+    background-color: lightskyblue;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,60 +5,38 @@
       <h1 class="title">
         vueapp
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+      <div class="grid">
+        <div v-for="item in dataList" :key="item.id">
+          <Item :itemData = 'item'/>
+
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import myData from '../data/MOCK_DATA.json';
+import Item from '@/components/Item.vue';
 
 export default {
   components: {
-    AppLogo
+    Item
+  },
+  data(){
+    return{
+      dataList: myData
+    }
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 20px;
+  }
 </style>
+
+
